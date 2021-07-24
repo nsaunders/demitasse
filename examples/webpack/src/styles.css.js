c75a4caaa4d5c2aa4c1d4fecc29caabe73117e9b
Nick Saunders <nick@saunde.rs>
2021-07-23 17:41:59 -0700

    Remove decache.

--- examples/webpack/src/styles.css.js
+++ examples/webpack/src/styles.css.js
@@ -5,8 +5,20 @@ require("ts-node").register({
 const modules = ["./App.ts"].map(require.resolve);
 
 module.exports = () => {
+
   require("demitasse").css = require("demitasse/extract").css;
-  const code = modules.map(require).map(({ styles }) => styles).flatMap(Object.values).join("\n");
-  modules.forEach(require("decache"));
+
+  const code =
+    modules
+      .map(require)
+      .map(({ styles }) => styles)
+      .flatMap(Object.values)
+      .join("\n");
+
+  modules.forEach(key => {
+    delete require.cache[key];
+  });
+
   return { code, dependencies: modules };
+
 };
