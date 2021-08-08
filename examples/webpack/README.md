3c7bd7801a066b6d1a83e3f9d96061717abda0eb
Nick Saunders <nick@saunde.rs>
2021-08-08 12:36:35 -0700

    Webpack documentation.

---
+++ examples/webpack/README.md
@@ -0,0 +1,49 @@
+# webpack example
+
+This example project demonstrates how to configure
+[webpack](https://webpack.js.org) to produce a static CSS file.
+
+## Libraries used
+1. [ts-node](https://github.com/TypeStrong/ts-node) offers a convenient way to
+   run TypeScript modules in a Node.js environment.
+2. [val-loader](https://webpack.js.org/loaders/val-loader) offers a way to
+   generate a module dynamically, i.e. to use a Node.js script to produce the
+   contents of a module.
+3. [postcss-loader](https://webpack.js.org/loaders/postcss-loader)
+   integrates [PostCSS](https://postcss.org) processing with Webpack. A common
+   use case is
+   [adding vendor prefixes](https://github.com/postcss/autoprefixer)
+   automatically without adding noise to your code.
+4. [css-loader](https://webpack.js.org/loaders/css-loader) converts the plain
+   CSS string content into the module format required by
+   [mini-css-extract-plugin](https://webpack.js.org/plugins/mini-css-extract-plugin)
+   (used in this project) or
+   [style-loader](https://webpack.js.org/loaders/style-loader) (not used here).
+5. [mini-css-extract-plugin](https://webpack.js.org/plugins/mini-css-extract-plugin)
+   extracts the output of
+   [css-loader](https://webpack.js.org/loaders/css-loader) into a static CSS
+   file separate from the main JavaScript bundle.
+6. [webpack-virtual-modules](http://github.com/sysgears/webpack-virtual-modules)
+   offers a way to create virtual modules that don't exist in the physical file
+   system.
+
+Note that, of these, val-loader is likely the only library that is strictly
+required for CSS extraction. Beyond that, you can choose whatever mix of tools
+best suits your use case and personal preferences.
+
+## How it works
+
+The [`./src/styles.ts`](./src/styles.ts) module exports the style rules used
+throughout the app. Essentially, it functions as an index of style rules.
+
+A [val-loader](https://webpack.js.org/loaders/val-loader)-compatible Node.js
+script uses the style rule index to generate static CSS. This script exists as
+a virtual module [`./src/styles.css`](./webpack.config.js#L43-L89) because its
+source seems most relevant in the context of webpack configuration, but it could
+just as easily inhabit its own physical module.
+
+The generated CSS output is then passed on to
+[postcss-loader](https://webpack.js.org/loaders/postcss-loader),
+[css-loader](https://webpack.js.org/loaders/css-loader), and finally
+[mini-css-extract-plugin](https://webpack.js.org/plugins/mini-css-extract-plugin)
+to be emitted in the form of a static CSS file.
--- examples/webpack/webpack.config.js
+++ examples/webpack/webpack.config.js
@@ -42,26 +42,26 @@ module.exports = ({ production }) => ({
     new HtmlPlugin({ title: "Webpack Example" }),
     new VirtualModulesPlugin({
       "./src/styles.css": `
-        if (!(".ts" in require.extensions)) {
+        if (!(".ts" in require.extensions)) { // [1]
           require("ts-node").register({
-            transpileOnly: true,
+            transpileOnly: true, // [2]
             compilerOptions: {
-              module: "commonjs",
+              module: "commonjs", // [3]
             },
           });
         }
 
-        require("demitasse").css = require("demitasse/extract").css;
+        require("demitasse").css = require("demitasse/extract").css; // [4]
 
         const stylesPath = require.resolve("./styles.ts");
 
         module.exports = () => {
-          const code =
+          const code = // [5]
             Object
               .values(require(stylesPath))
               .flatMap(x => {
-                switch (typeof x) {
-                  case "object":
+                switch (typeof x) { // [6]
+                  case "object": // [7]
                     return Object.values(x);
                   case "string":
                     return [x];
@@ -71,7 +71,7 @@ module.exports = ({ production }) => ({
               })
               .join("\\n");
 
-          const dependencies =
+          const dependencies = // [8]
             module
               .children
               .filter(({ filename }) => filename.endsWith("/styles.ts"))
@@ -80,11 +80,43 @@ module.exports = ({ production }) => ({
               .concat(stylesPath);
 
           dependencies.forEach(key => {
-            delete require.cache[key];
+            delete require.cache[key]; // [9]
           });
 
           return { code, dependencies };
+
         };
+
+        /*
+        1. ts-node should be registered once, if *.ts files aren't already in
+           the list of supported "require" extensions.
+        2. Type-checking is most likely redundant in this context.
+        3. CommonJS is the module required in the remainder of the script.
+        4. Redirect css function calls to the demitasse/extract module, which
+           produces CSS code instead of class names.
+        5. Gather CSS code from the style index. The Object.values function is
+           used because the keys in the style index aren't relevant when
+           producing a CSS file for an entire app. (The keys may be relevant,
+           however, when building a component library: They can be used for
+           filenames in a CSS-file-per-component model. But that is well
+           outside the scope of this example project.)
+        6. Because of how the style index is structured, each value may
+           constitute a single ruleset or multiple. It is possible to
+           structure the style index differently if you would prefer to
+           move this bit of complexity elsewhere.
+        7. x would be an object when using the multiple-ruleset css
+           API, explained in the demitasse README. At runtime, the
+           keys map to generated class names. However, the generated
+           class names aren't relevant here, hence the use of
+           Object.values to strip them away.
+        8. The dependencies consist of all of the modules contributing to the
+           code produced by this script. These are required by val-loader, as
+           well as for cache invalidation.
+        9. It is necessary to remove the dependencies from the Node.js require
+           cache; otherwise, style changes won't be reflected in the next
+           build (for example, when using the webpack development server or
+           otherwise in watch mode).
+        */
       `,
     }),
     new MiniCssExtractPlugin(),
