225dc4e2a0b18f762fc56d5d9f3df2526fa6d83c
Nick Saunders <nick@saunde.rs>
2021-11-13 20:46:25 -0700

    Fix styles.ts format in esbuild example.

--- examples/esbuild/src/styles.ts
+++ examples/esbuild/src/styles.ts
@@ -3,10 +3,12 @@ import { css as example } from "./Example";
 import { css as input } from "./Input";
 import { sheets } from "demitasse";
 
-export default Object.values(
-  sheets([
-    ...button,
-    ...example,
-    ...input
-  ])
-);
+export default Object
+  .values(
+    sheets([
+      ...button,
+      ...example,
+      ...input
+    ])
+  )
+  .join("\n");
