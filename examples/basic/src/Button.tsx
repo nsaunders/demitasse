435686d44b6ce31d2ee04f9be5fcafae7b88d99d
Nick Saunders <nick@saunde.rs>
2021-11-15 20:18:37 -0700

    Update basic example to 2.1.0

--- examples/basic/src/Button.tsx
+++ examples/basic/src/Button.tsx
@@ -1,10 +1,10 @@
 import { FC, HTMLAttributes } from "react";
-import { demi, cssExport } from "demitasse";
+import { cssRules, cssExport } from "demitasse";
 import cx from "classnames";
 
 const cssModuleId = "button";
 
-const [buttonCSS, styles] = /*#__PURE__*/ demi(cssModuleId, {
+const [_css, styles] = /*#__PURE__*/ cssRules(cssModuleId, {
   appearance: "none",
   outline: "none",
   paddingTop: 4,
@@ -25,4 +25,4 @@ export const Button: FC<HTMLAttributes<HTMLButtonElement>> = ({ className, ...pr
   <button className={cx(className, styles)} {...props} />
 );
 
-export const css = /*#__PURE__*/ cssExport(cssModuleId, buttonCSS);
+export const css = /*#__PURE__*/ cssExport(cssModuleId, _css);
