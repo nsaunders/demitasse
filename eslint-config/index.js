f96893c49255e188cb2895a6e8d9a3b0c622985e
Nick Saunders <nick@saunde.rs>
2023-01-16 15:35:30 -0700

    Code quality stuff

---
+++ eslint-config/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
