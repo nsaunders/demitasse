78d381055f8133380acc6ca0d1c76d32bbabe38e
Nick Saunders <nick@saunde.rs>
2023-01-18 06:57:02 -0700

    Delete v2

--- examples/webpack/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { createElement } from "react";
-import { render } from "react-dom";
-import { Example } from "./Example"; 
-
-const host = document.createElement("main");
-document.body.appendChild(host);
-render(createElement(Example), host);
