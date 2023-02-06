60fd3cb865dc62f28610f8cb81cf78b908172d6a
Nick Saunders <nick@saunde.rs>
2023-02-06 12:54:02 -0700

    Hook example, drop PostCSS dependency.

--- examples/hook/src/useCSS.ts
+++ examples/hook/src/useCSS.ts
@@ -1,9 +1,7 @@
 import type { cssBindings } from "demitasse";
 import { makeCSSBindings } from "demitasse"; // eslint-disable-line no-duplicate-imports
 import { useEffect, useMemo, useState } from "preact/hooks";
-import postcss from "postcss";
-import prefixer from "postcss-prefixer";
-import prefixKeyframe from "postcss-prefix-keyframe";
+import { compile, serialize, stringify, middleware } from "stylis";
 
 const useCSS: typeof cssBindings = css => {
   const [prefix] = useState(
@@ -12,11 +10,64 @@ const useCSS: typeof cssBindings = css => {
         .toString(36)
         .replace(/^[0-9]/, x => `x${x}`)}___`,
   );
-  const scopedCSS = useMemo(() => {
-    return postcss(
-      [prefixer, prefixKeyframe].map(plugin => plugin({ prefix })),
-    ).process(css).css;
-  }, [prefix, css]);
+  const scopedCSS = useMemo(
+    () =>
+      serialize(
+        compile(css),
+        middleware([
+          (element, index, children, callback) => {
+            switch (element.type) {
+              case "rule":
+                switch (typeof element.props) {
+                  case "object":
+                    element.props = element.props.map(prop =>
+                      prop.replace(/[.#]/g, x => `${x}${prefix}`),
+                    );
+                    break;
+                  case "string":
+                    element.props = element.props.replace(
+                      /[.#]/g,
+                      x => `${x}${prefix}`,
+                    );
+                    break;
+                }
+                break;
+              case "@keyframes":
+                switch (typeof element.props) {
+                  case "object":
+                    element.value = `@keyframes ${prefix}${element.props[0]}`;
+                    break;
+                  case "string":
+                    element.value = `@keyframes ${prefix}${element.props}`;
+                    break;
+                }
+                break;
+              case "decl":
+                switch (element.props) {
+                  case "animation":
+                    (css.match(/@keyframes[^{]+/g) || [])
+                      .map((x: string) => x.split(/\s+/).slice(1)[0])
+                      .reduce(
+                        (value: string, name: string) =>
+                          value.replace(
+                            new RegExp(`(^|\\s)(${name})(\\s|$)`),
+                            (a, _b, c) => a.replace(c, `${prefix}${c}`),
+                          ),
+                        element.value,
+                      );
+                    break;
+                  case "animation-name":
+                    element.value = `${element.props}: ${prefix}${element.children}`;
+                    break;
+                }
+                break;
+            }
+          },
+          stringify,
+        ]),
+      ),
+    [prefix, css],
+  );
   useEffect(() => {
     const style = document.createElement("style");
     style.textContent = scopedCSS;
